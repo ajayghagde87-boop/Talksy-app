f18e27162968123ba3984f11ad23b5026518c68b
Ajay Ghagde <ajayghagde87@gmail.com>
2026-01-13 17:23:22 +0530

    inngest error fixed

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import 'dotenv/config'
 import connectDB from './config/db.js';
 import { inngest, functions } from './inngest/index.js'
-// import {serve} from 'inngest/express'
+import {serve} from 'inngest/express'
 // import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
